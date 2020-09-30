fdb1ed1b71a339d61761412a43df9554ffe4c9dc
Wayne Beaton <wayne.beaton@eclipse-foundation.org>
2020-09-30 18:29:54 -0400

    Initial commit.
    Signed-off-by: Wayne Beaton <wayne.beaton@eclipse-foundation.org>

---
+++ README.md
@@ -0,0 +1,5 @@
+# Eclipse Foundation Working Groups
+
+This repository contains the documents that define the Eclipse Foundation Working Group Process.
+
+All rights reserved.
---
+++ source/convert.sh
@@ -0,0 +1,7 @@
+sed 'i/\+$/\n/g'
+
+heading=h.1amhxlfy6u5 > 
+
+#heading=h\.ikaz1pn6qp0i\[([^\]]+)\] <<specification-committee,\1>>
+#heading=h.s0ll6ht21dgb\[([^\]]+)\] <<mailing-lists,\1>>
+#heading=h.eif127d9bafm\[([^\]]+)\] <<proposal-phase,\1>>
